47245462703ba3737c6c4ffb2086b885daffb27d
nikola369nikolaev <nikola_nikolaev1@abv.bg>
2024-10-09 19:12:04 +0300

    Clock after 30 mins ex

---
+++ zad1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int hours = int.Parse(Console.ReadLine());
+int mins = int.Parse(Console.ReadLine());
+
+mins = mins + 30;
+
+if (mins>=60)
+{
+    mins = mins - 60;
+    hours++;
+}
+
+if (hours==24)
+{
+    hours = 0;
+}
+Console.WriteLine($"{hours}:{mins:D2}");
+
